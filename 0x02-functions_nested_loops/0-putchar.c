28e73a5d3afa7e46b47bc40774b57a5eff941f53
Mariams-coding-diary <m.hassan70190@yahoo.com>
2025-04-05 13:59:40 -0700

    forgot the new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,5 +10,5 @@
 int main(void)
 {
 	my_function();
-	return 0;
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 static void my_function(void){
-	write(1, "_putchar", 8);
+	write(1, "_putchar\n", 9);
 }
 
 #endif
